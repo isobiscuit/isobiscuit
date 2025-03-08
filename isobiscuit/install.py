1a3e1da84f7605febe749c5c8c80dd584b0c6d33
trollmii <trollmii@outlook.de>
2025-03-08 14:39:00 +0100

    added in biscuit file mode for cli

--- isobiscuit/install.py
+++ isobiscuit/install.py
@@ -9,16 +9,21 @@ def main():
     import sys
     import os
     biscuit = sys.argv[1]
+    biscuit_with_params = sys.argv[2]
+    args_with_params = sys.argv[2:]
     args = sys.argv[1:]
     in_biscuit_folder = False
     if os.path.exists("biscuit.yml"):
         biscuit = "."
+        biscuit_with_params = "."
         args = sys.argv[0:]
+        args_with_params = sys.argv[1:]
         in_biscuit_folder = True
+        
     if sys.argv[1] == "-u":
-        update(biscuit, args[0])
+        update(biscuit_with_params, args_with_params[0])
     elif sys.argv[1] == "-R":
-        remove(biscuit, args[0])
+        remove(biscuit_with_params, args_with_params[0])
     else:
         installFunc(biscuit, args[0])
 
