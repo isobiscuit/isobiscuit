6e979c9e85d8ba5a5597a907d0220eed474f41af
trollmii <trollmii@outlook.de>
2025-03-12 18:29:33 +0100

    performance

--- isobiscuit/__init__.py
+++ isobiscuit/__init__.py
@@ -2,6 +2,6 @@ import warnings
 
 warnings.filterwarnings("ignore", category=UserWarning, module=r"zipfile")
 
-from . import cli, api, compiler, run
+from . import cli, compiler, run
 from .cli import build_biscuit, init_biscuit
 from .compiler import buildBiscuit
